4a280cf3d9d647fe863d12ddeafe34b2ea452d57
gersmann <stefan@gersmann.de>
2024-01-15 15:32:10 +0100

    Implements a MongoDB backend for Django, which supports CRUD operations, relations, single-table inheritance and single-table OneToOne relations.
    Not implemented yet are joins, aggregations (other than count), and embedded document arrays.

---
+++ testapp/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.db.models import JSONField
+
+from django_mongodb.managers import MongoManager
+
+
+class FooModel(models.Model):
+    objects: MongoManager = MongoManager()
+
+    json_field = JSONField()
+    name = models.CharField(max_length=100)
+    datetime_field = models.DateTimeField(auto_now_add=True)
+    time_field = models.TimeField(auto_now_add=True)
+    date_field = models.DateField(auto_now_add=True)
+
+
+class SameTableChild(FooModel):
+    dummy_model_ptr = models.OneToOneField(
+        FooModel,
+        on_delete=models.CASCADE,
+        parent_link=True,
+        related_name="child_type",
+        db_column="_id",
+    )
+    extended = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "testapp_foomodel"
+
+
+class SameTableOneToOne(models.Model):
+    dummy_model = models.OneToOneField(
+        FooModel,
+        primary_key=True,
+        on_delete=models.CASCADE,
+        related_name="extends",
+        db_column="_id",
+    )
+    extra = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "testapp_foomodel"
+
+
+class DifferentTableOneToOne(models.Model):
+    dummy_model = models.OneToOneField(
+        FooModel,
+        primary_key=True,
+        on_delete=models.CASCADE,
+        related_name="another_extends",
+        db_column="_id",
+    )
+    extra = models.CharField(max_length=100)
+
+
+class RelatedModel(models.Model):
+    name = models.CharField(max_length=100)
+    foo = models.ForeignKey(FooModel, on_delete=models.CASCADE, related_name="related")
